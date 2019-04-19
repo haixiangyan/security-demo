0bfdea217513eb360d46b521bd0fa2f4cab0b53e
Haixiang <haixiang6123@gmail.com>
2019-04-19 11:45:00 -0700

    Add pages

---
+++ src/pages/CSRF.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class CSRF extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="CSRF">CSRF</div>
+        )
+    }
+}
+
+export default CSRF
---
+++ src/pages/XSS.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class XSS extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="XSS">XSS</div>
+        )
+    }
+}
+
+export default XSS

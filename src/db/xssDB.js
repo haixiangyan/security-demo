3d4e10980e7697ebdc676d37b690698a02840afe
Haixiang <haixiang6123@gmail.com>
2019-04-23 09:21:47 -0700

    Add left layout of XSS page

---
+++ src/db/xssDB.js
@@ -0,0 +1,23 @@
+export const db = [{
+    key: '1',
+    name: 'Jack',
+    age: 32,
+}, {
+    key: '2',
+    name: 'Tom',
+    age: 42,
+}, {
+    key: '3',
+    name: 'Jerry',
+    age: 12,
+}]
+
+export const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+}, {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+}];
--- src/pages/XSS.js
+++ src/pages/XSS.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react'
+import { Input, Row, Col, Divider, Table } from 'antd';
+import { db, columns } from "../db/xssDB"
+
+const Search = Input.Search;
 
 class XSS extends Component {
     constructor(props) {
@@ -7,7 +11,28 @@ class XSS extends Component {
 
     render() {
         return (
-            <div className="XSS">XSS</div>
+            <Row gutter={16} type="flex" justify="center" className="XSS">
+                <Col span={8}>
+                    <Divider>Search boxes</Divider>
+                    <p>Search box without detecting XSS attack</p>
+                    <Search
+                        placeholder="Enter name or age to search (Not detect XSS attack)"
+                        onSearch={value => console.log(value)}
+                        enterButton
+                    />
+                    <p>Search box with detecting XSS attack</p>
+                    <Search
+                        placeholder="Enter name or age to search (Detect XSS attack)"
+                        onSearch={value => console.log(value)}
+                        enterButton
+                    />
+                    <Divider>Database</Divider>
+                    <Table dataSource={db} columns={columns} />
+                </Col>
+                <Col span={8}>
+                    Program
+                </Col>
+            </Row>
         )
     }
 }

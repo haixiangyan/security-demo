2811ca1dc6c7c747308f1aa857776cfe9f34977d
Haixiang <haixiang6123@gmail.com>
2019-04-24 10:09:43 -0700

    Add XSS page

---
+++ src/pages/XSS/XSS.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class XSS extends Component {
+    render() {
+        return (
+            <div className="xss">
+                xss
+            </div>
+        )
+    }
+}
+
+export default XSS

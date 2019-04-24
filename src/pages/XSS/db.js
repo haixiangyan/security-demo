34136f859cba8c8006a3cd4b2e74232e17e8df86
Haixiang <haixiang6123@gmail.com>
2019-04-24 10:16:26 -0700

    Add review db

---
+++ src/pages/XSS/db.js
@@ -0,0 +1,19 @@
+export const db = [{
+    key: '1',
+    id: '1',
+    content: 'A nice movie',
+}, {
+    key: '2',
+    id: '2',
+    content: 'That movie is so fun',
+}]
+
+export const columns = [{
+    title: 'review_id',
+    dataIndex: 'id',
+    key: 'id',
+}, {
+    title: 'review_content',
+    dataIndex: 'content',
+    key: 'content',
+}];

